6ac71ad4ef1ec4c4624b6bbd0de8fa187642b0d3
aldhi saputra <aldhisaputra795@gmail.com>
2024-03-09 21:07:16 +0700

    menambahkan fragmen & layout Sign Up Address

--- app/src/main/java/com/example/foodmarket/ui/SignUpAddressFragment.kt
+++ app/src/main/java/com/example/foodmarket/ui/SignUpAddressFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.foodmarket.R
 
-
-class SignUpFragment : Fragment() {
+class SignUpAddressFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        return inflater.inflate(R.layout.fragment_sign_up_address, container, false)
     }
+
 }

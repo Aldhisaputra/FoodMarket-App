de486eebb1ecfc15d1912e4683e8deb5329a82e8
aldhi saputra <aldhisaputra795@gmail.com>
2024-03-19 23:07:09 +0700

    menambahkan navigation folder & AuthActivity

---
+++ app/src/main/java/com/example/foodmarket/ui/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.foodmarket.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.foodmarket.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}

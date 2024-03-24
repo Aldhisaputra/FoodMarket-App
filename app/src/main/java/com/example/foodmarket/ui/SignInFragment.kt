eb9213cbae1203a249d3a9885572138dfcf0655f
aldhi saputra <aldhisaputra795@gmail.com>
2024-03-24 22:20:32 +0700

    menambahkan plugin gradle id(kotlin-android-extensions)

--- app/src/main/java/com/example/foodmarket/ui/SignInFragment.kt
+++ app/src/main/java/com/example/foodmarket/ui/SignInFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.foodmarket.R
+import kotlinx.android.synthetic.main.fragment_signin.*
 
 
 class SignInFragment : Fragment() {
@@ -16,4 +17,5 @@ class SignInFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_signin, container, false)
     }
+
 }
